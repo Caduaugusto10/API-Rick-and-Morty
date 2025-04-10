a82a5cd61d6b4ccde4aa5b136d46a914b5ab0981
Carlos Eduardo Ferraz Augusto <cadu.augusto10@gmail.com>
2025-04-10 11:15:48 -0300

    feat: create api of rick and morty in front-end

---
+++ app/home/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import styles from "./Home.module.css"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import CharacterCard from "../../components/CharacterCard"
+
+export default function Home() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('https://rickandmortyapi.com/api/character')
+            .then((response) => {
+            setCharacters(response.data.results)
+        })
+        .catch((error) => {
+            console.error("Erro ao buscar personagens:", error);
+        });
+    }, []);
+
+    console.log(characters)
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.grid}>
+                {characters.map((char) => (
+                <CharacterCard key={char.id} character={char} />
+            ))}
+            </div>
+        </div>
+    )
+}
